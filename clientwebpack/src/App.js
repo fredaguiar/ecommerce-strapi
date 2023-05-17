9e25d9009ee551c60673d74f5929efda4e63ae2d
Frederico Teixeira <fredaguiar@Fredericos-MBP.lan>
2023-05-17 11:58:40 -0700

    webpack

---
+++ clientwebpack/src/App.js
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className='app'>HELLO !!!!</div>;
+};
+
+export default App;
---
+++ clientwebpack/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, '/dist'),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        },
+        exclude: [/node_modules/]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true, // cache busting
+      template: './src/index.html'
+    })
+  ]
+};
