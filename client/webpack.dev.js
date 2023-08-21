2b634ad9d2a10cfa06b7e01736238f7d00b07b56
Frederico Teixeira <fredaguiar@Fredericos-MacBook-Pro.local>
2023-08-21 14:13:02 -0700

    Fix Router 404

--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -3,13 +3,21 @@ const webpackConfig = require('./webpack.config.js');
 const Dotenv = require('dotenv-webpack');
 const path = require('path');
 
+/**
+ * Fix routing issues where the router returns 404 on refresh/reload when
+ * there is a path http://localhost:8080/path/to/somewhere
+ *  publicPath: '/'
+ *  historyApiFallback: true
+ */
+
 module.exports = merge(webpackConfig, {
   mode: 'development',
   entry: './src/index.tsx',
   output: {
     filename: 'bundle.dev.js',
     path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'images/[hash][ext][query]'
+    assetModuleFilename: 'images/[hash][ext][query]',
+    publicPath: '/'
   },
   devtool: 'source-map',
   module: {
@@ -19,6 +27,9 @@ module.exports = merge(webpackConfig, {
         use: ['style-loader', 'css-loader']
       }
     ]
+  },
+  devServer: {
+    historyApiFallback: true
   },
   plugins: [new Dotenv()]
 });
