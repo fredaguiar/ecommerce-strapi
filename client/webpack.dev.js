45f1e511d3877489c0095b22cf422ea48b715894
fredaguiar <fredaguiar@FTeixeira>
2023-09-21 11:50:27 -0700

    server prod without .env

--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -1,7 +1,7 @@
-const { merge } = require('webpack-merge');
-const webpackConfig = require('./webpack.config.js');
-const Dotenv = require('dotenv-webpack');
-const path = require('path');
+const { merge } = require("webpack-merge");
+const webpackConfig = require("./webpack.config.js");
+const Dotenv = require("dotenv-webpack");
+const path = require("path");
 
 /**
  * Fix routing issues where the router returns 404 on refresh/reload when
@@ -11,25 +11,26 @@ const path = require('path');
  */
 
 module.exports = merge(webpackConfig, {
-  mode: 'development',
-  entry: './src/index.tsx',
+  mode: "development",
+  entry: "./src/index.tsx",
   output: {
-    filename: 'bundle.dev.js',
-    path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'images/[hash][ext][query]',
-    publicPath: '/'
+    filename: "bundle.dev.js",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "images/[hash][ext][query]",
+    publicPath: "/",
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   module: {
     rules: [
       {
         test: /\.(css)$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
   devServer: {
-    historyApiFallback: true
+    port: 3000,
+    historyApiFallback: true,
   },
-  plugins: [new Dotenv()]
+  plugins: [new Dotenv()],
 });
